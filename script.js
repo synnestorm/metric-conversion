c83d5513283291620ac23441a237b967acd83c00
synnestorm <synne.storm@gmail.com>
2025-08-29 16:21:17 +0200

    let variables

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
+// variables
+let meter = 3.281
+let liter = 0.264
+let kilogram = 2.204
+
 const volumeEl = document.getElementById("volume")
 const massEl = document.getElementById("mass")
 const lengthEl = document.getElementById("length")
