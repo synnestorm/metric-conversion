201109a076c205c3880f9bb64c3b7733bb6daf90
synnestorm <synne.storm@gmail.com>
2025-08-30 11:04:59 +0200

    .toFixed

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ const convertBtn = document.getElementById("convert-btn")
 convertBtn.addEventListener("click", function() {
     const inputValue = Number(inputCon.value)
 
-    const metersToFeet = inputValue * 3.281
-    const feetToMeters = inputValue * 0.3048
+    const metersToFeet = inputValue * 3.281.toFixed(2)
+    const feetToMeters = inputValue * 0.3048.toFixed(2)
 
-    const litersToGallons = inputValue * 0.264
-    const gallonsToLiters = inputValue * 3.785
+    const litersToGallons = inputValue * 0.264.toFixed(2)
+    const gallonsToLiters = inputValue * 3.785.toFixed(2)
 
-    const kilosToPounds = inputValue * 2.204
-    const poundsToKilos = inputValue * 0.4535
+    const kilosToPounds = inputValue * 2.204.toFixed(2)
+    const poundsToKilos = inputValue * 0.4535.toFixed(2)
 
     lengthEl.textContent = `${metersToFeet} meters | ${feetToMeters} feet`
     massEl.textContent = `${kilosToPounds} kilos | ${poundsToKilos} pounds`
