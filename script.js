15ff55426ab9fca7f7340f41bf7075558ae668b7
synnestorm <synne.storm@gmail.com>
2025-08-30 11:16:46 +0200

    .textContent updated

--- script.js
+++ script.js
@@ -9,16 +9,19 @@ const convertBtn = document.getElementById("convert-btn")
 convertBtn.addEventListener("click", function() {
     const inputValue = Number(inputCon.value)
 
-    const metersToFeet = inputValue * 3.281.toFixed(2)
-    const feetToMeters = inputValue * 0.3048.toFixed(2)
+    const metersToFeet = inputValue * 3.281
+    const feetToMeters = inputValue * 0.3048
 
-    const litersToGallons = inputValue * 0.264.toFixed(2)
-    const gallonsToLiters = inputValue * 3.785.toFixed(2)
+    const litersToGallons = inputValue * 0.264
+    const gallonsToLiters = inputValue * 3.785
 
-    const kilosToPounds = inputValue * 2.204.toFixed(2)
-    const poundsToKilos = inputValue * 0.4535.toFixed(2)
+    const kilosToPounds = inputValue * 2.204
+    const poundsToKilos = inputValue * 0.4535
 
-    lengthEl.textContent = `${metersToFeet} meters | ${feetToMeters} feet`
-    massEl.textContent = `${kilosToPounds} kilos | ${poundsToKilos} pounds`
-    volumeEl.textContent = `${litersToGallons} liters | ${gallonsToLiters} gallons`
+    lengthEl.textContent = `${inputValue} meters = ${metersToFeet.toFixed(2)} feet
+                                 | ${inputValue} feet = ${feetToMeters.toFixed(2)} meters`
+    massEl.textContent = `${inputValue} kilos = ${kilosToPounds.toFixed(2)} pounds
+                                     | ${inputValue} pounds = ${poundsToKilos.toFixed(2)} kilos`
+    volumeEl.textContent = `${inputValue} liters = ${litersToGallons.toFixed(2)} gallons
+                                         | ${inputValue} gallons = ${gallonsToLiters.toFixed(2)} liters`
 })
